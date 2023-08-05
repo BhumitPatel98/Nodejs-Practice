9fc6e6d186222a79e6062b078dec70e7c90226d3
BhumitPatel98 <bhumitpatel58@gmail.com>
2023-08-05 17:07:56 +0530

    second commit

--- first-app.js
+++ first-app.js
@@ -2,11 +2,10 @@
 // fs.writeFileSync('hello.txt','hello from Node.js')
 
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes')
+const app = express();
 
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
